1c2691f224d88bbef735824f3bff9e2e29eece01
lincw <56734434+quul@users.noreply.github.com>
2024-12-17 18:17:57 +0800

    feat: next-auth

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,7 +1,6 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
-
+import GitHub from "next-auth/providers/github";
 import { db } from "@/server/db";
 
 /**
@@ -32,7 +31,7 @@ declare module "next-auth" {
  */
 export const authConfig = {
   providers: [
-    DiscordProvider,
+    GitHub,
     /**
      * ...add more providers here.
      *
